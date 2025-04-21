1ae66c0799bee14ff2669bf10fce1bef29b0995e
Kingjibes <160682090+Kingjibes@users.noreply.github.com>
2025-04-21 22:48:46 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  PREFIX: process.env.PREFIX || "CIPHER-MD*7",
+  EMAIL: process.env.EMAIL || "richvybs92@gmail.com",
+  PASS: process.env.PASS || "Fuckyou2025$",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id with your girlfriend",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
