3c511cd0d98590900dfd98824cf243d3b9977436
Kingjibes <160682090+Kingjibes@users.noreply.github.com>
2025-04-23 00:00:32 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
   PREFIX: process.env.PREFIX || "CIPHER-MD*7",
   EMAIL: process.env.EMAIL || "richvybs92@gmail.com",
-  PASS: process.env.PASS || "Fuckyou2025$",
+  PASS: process.env.PASS || "Fuckyou2035$",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id with your girlfriend",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
